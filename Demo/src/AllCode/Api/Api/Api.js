298a3e9665163d4fa579b9abc42c3b2087659e87
SantoshAnimesh <santoshanimesh76.sk@gmail.com>
2022-05-19 23:01:42 +0530

    updateAll

--- Demo/src/AllCode/Api/Api/Api.js
+++ Demo/src/AllCode/Api/Api/Api.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Axios from "axios";
+//import Axios from "axios";
 import "./App.css";
 function Api() {
   const [data, setData] = useState([]);
